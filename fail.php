f406e0e3cc89f724d7330fd717a3670e1944a6eb
Matokosp <jmsalinas@uc.cl>
2021-01-18 17:05:34 -0600

    contact messages

--- fail.php
+++ fail.php
@@ -1,1 +1,43 @@
-FAIL
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Arrayan Assets</title>
+        <meta name="description" content="">
+        <!-- FONTS -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Martel:wght@400;600;700&family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- MY STYLES -->
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+        <link rel="stylesheet" href="assets/css/menu.css">
+        <!-- FAVICONS -->
+        <link href="assets/ico/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="assets/ico/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
+        <link href="assets/ico/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
+        <link href="assets/ico/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+        <link href="assets/ico/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="assets/ico/icon-hires.png" rel="icon" sizes="192x192" />
+        <link href="assets/ico/icon-normal.png" rel="icon" sizes="128x128" />
+        <link href="assets/ico/icon.svg" rel="mask-icon" sizes="any" color="#61b94c">
+    </head>
+    <body>
+    <section class="contact-message" id="sectionOne">
+        <div class="container">
+            <img class="contact-message-icon" src="assets/images/fail.png" alt="">
+            <h2>
+                Se ha producido un error al enviar tu mensaje. <br>
+                Inténtalo nuevamente.
+            </h2>
+        </div>
+        <a class="home-back-button" href="index.php">volver a home</a>
+    </section>
+    <footer>
+        <img src="assets/images/arrayan-logo.png" alt="">
+        <div>
+            <h6>ArrayanAsset® 2018© <br>
+                Todos los Derechos Reservados.</h6>
+        </div>
+    </footer>    
+    </body>
+</html>
